9a7d143beb70d71a85f74d444c7ee2f1a0df0520
mfarooq.mc <mfarooq.mc@emaar.ae>
2021-06-10 15:42:03 +0400

    Map fragment and basic navigation completed.

--- app/src/main/java/com/capricorn/carslist/ui/component/MapFragment.kt
+++ app/src/main/java/com/capricorn/carslist/ui/component/MapFragment.kt
@@ -8,13 +8,19 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.capricorn.carslist.R
 import com.capricorn.carslist.databinding.FragmentMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class MapFragment : Fragment() {
+class MapFragment : Fragment(),OnMapReadyCallback {
 
     private var _binding: FragmentMapBinding? = null
+    private lateinit var mapView: MapView
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,6 +32,14 @@ class MapFragment : Fragment() {
     ): View? {
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
+
+        //getting the map view here
+        mapView = binding.map
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync(this)
+
+
         return binding.root
 
     }
@@ -36,10 +50,59 @@ class MapFragment : Fragment() {
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_MapFragment_to_ListFragment)
         }
+
+
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY) ?: Bundle().also {
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, it)
+        }
+        mapView.onSaveInstanceState(mapViewBundle)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.let {
+            it.onResume()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.let {
+            it.onPause()
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        mapView.let {
+            it.onDestroy()
+        }
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.let {
+            it.onLowMemory()
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        googleMap ?: return
+        with(googleMap) {
+            //moveCamera(CameraUpdateFactory.newLatLngZoom(,ZOOM_LEVEL))
+            //addMarker(MarkerOptions().position(SYDNEY))
+        }
+    }
+
+
+    companion object {
+        private const val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
+        val ZOOM_LEVEL = 13f
     }
 }
---
+++ app/src/main/java/com/capricorn/carslist/utils/LatLongUtils.kt
@@ -0,0 +1,7 @@
+package com.capricorn.carslist.utils
+
+/**
+ * Created by Muhammad Umar on 10/06/2021.
+ */
+class LatLongUtils {
+}
